d876c26c8f4fdf2b5ed0c2379ca76a6ff6a58dbc
ellyj0409 <120316454+ellyj0409@users.noreply.github.com>
2022-12-13 08:10:43 +0900

    Update final_project12.py

--- final_project12.py
+++ final_project12.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 from PIL import Image
-import matplotlib.pyplot as plt
 
 def load_image(image_file):
     img = Image.open(image_file)
